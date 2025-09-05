723d1386f0159fa8e04f231f4de10756049edddd
Omar Elrfaay <omar.tolis2004@gmail.com>
2025-09-05 16:59:26 +0300

    Add BaseEntity class for common entity attributes and lifecycle methods

---
+++ src/main/java/com/weather_found/weather_app/modules/shared/Base/BaseEntity.java
@@ -0,0 +1,36 @@
+package com.weather_found.weather_app.modules.shared.Base;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Data;
+
+@Data
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(nullable = false)
+    private Instant updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = updatedAt = Instant.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = Instant.now();
+    }
+}
