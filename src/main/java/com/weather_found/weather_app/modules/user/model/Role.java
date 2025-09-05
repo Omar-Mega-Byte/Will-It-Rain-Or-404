0ec6110ffc3de3c1b82d5baf2621bb2603d5e407
Omar Elrfaay <omar.tolis2004@gmail.com>
2025-09-05 17:12:41 +0300

    Refactor Role class to remove Lombok annotations and add explicit constructors, getters, equals, hashCode, and toString methods

--- src/main/java/com/weather_found/weather_app/modules/user/model/Role.java
+++ src/main/java/com/weather_found/weather_app/modules/user/model/Role.java
@@ -5,17 +5,73 @@ import com.weather_found.weather_app.modules.shared.Base.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 @Entity
 @Table(name = "roles")
-@Data
-@EqualsAndHashCode(callSuper = true)
 public class Role extends BaseEntity {
     @Column(nullable = false, unique = true)
     private String name;
 
     @Column(nullable = false)
     private String description;
+
+    // Default constructor
+    public Role() {
+        super();
+    }
+
+    // Constructor with all fields
+    public Role(String name, String description) {
+        super();
+        this.name = name;
+        this.description = description;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        if (!super.equals(obj)) return false;
+        
+        Role role = (Role) obj;
+        
+        if (name != null ? !name.equals(role.name) : role.name != null) return false;
+        return description != null ? description.equals(role.description) : role.description == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + getId() +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", createdAt=" + getCreatedAt() +
+                ", updatedAt=" + getUpdatedAt() +
+                '}';
+    }
 }
