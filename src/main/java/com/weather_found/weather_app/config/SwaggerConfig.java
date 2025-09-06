e8e59cd3b46eca4362d92844dd9481c1e3ffbe76
Omar Elrfaay <omar.tolis2004@gmail.com>
2025-09-06 17:21:41 +0300

    Enhance Swagger configuration with comprehensive API documentation, contact info, and JWT bearer token authentication

--- src/main/java/com/weather_found/weather_app/config/SwaggerConfig.java
+++ src/main/java/com/weather_found/weather_app/config/SwaggerConfig.java
@@ -2,17 +2,37 @@ package com.weather_found.weather_app.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Weather App API")
                         .version("1.0")
-                        .description("API documentation for Weather application"));
+                        .description("Comprehensive API documentation for Weather Prediction Application - Will It Rain On My Parade?")
+                        .contact(new Contact()
+                                .name("Weather App Support")
+                                .email("support@weatherapp.com")
+                                .url("https://github.com/Omar-Mega-Byte"))
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("JWT Bearer Token Authentication")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 }
