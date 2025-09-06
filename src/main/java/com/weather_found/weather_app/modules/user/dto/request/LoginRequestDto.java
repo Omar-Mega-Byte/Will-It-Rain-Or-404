b2b02496714f069252c8d8b324f69d99be62d716
Omar Elrfaay <omar.tolis2004@gmail.com>
2025-09-06 17:22:22 +0300

    Refactor LoginRequestDto to remove Lombok dependency and explicitly define fields and methods

--- src/main/java/com/weather_found/weather_app/modules/user/dto/request/LoginRequestDto.java
+++ src/main/java/com/weather_found/weather_app/modules/user/dto/request/LoginRequestDto.java
@@ -1,12 +1,22 @@
 package com.weather_found.weather_app.modules.user.dto.request;
 
-import lombok.Value;
-
 /**
  * Immutable DTO for user login requests
  */
-@Value
 public class LoginRequestDto {
-    String username;
-    String password;
+    private final String username;
+    private final String password;
+
+    public LoginRequestDto(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
