4fdde34cb064bfc47a8d3d8a70bcac9f226d4b2f
Omar Elrfaay <omar.tolis2004@gmail.com>
2025-09-06 17:22:56 +0300

    Refactor UserSummaryDto to remove Lombok dependency and enhance readability

--- src/main/java/com/weather_found/weather_app/modules/user/dto/response/UserSummaryDto.java
+++ src/main/java/com/weather_found/weather_app/modules/user/dto/response/UserSummaryDto.java
@@ -1,18 +1,42 @@
 package com.weather_found.weather_app.modules.user.dto.response;
 
-import lombok.Value;
-
 /**
  * Immutable DTO for lightweight user information in lists or references
  * Contains minimal fields for performance and privacy
  */
-@Value
 public class UserSummaryDto {
 
-    Long id;
-    String username;
-    String firstName;
-    String lastName;
-    boolean isActive;
+    private final Long id;
+    private final String username;
+    private final String firstName;
+    private final String lastName;
+    private final boolean isActive;
 
+    public UserSummaryDto(Long id, String username, String firstName, String lastName, boolean isActive) {
+        this.id = id;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.isActive = isActive;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
 }
