a8e130b1082e73dd31137aabfc32444583d7d7c8
Omar Elrfaay <omar.tolis2004@gmail.com>
2025-09-06 23:09:56 +0300

    Add EventStatus and EventType enums for event categorization

---
+++ src/main/java/com/weather_found/weather_app/modules/event/model/enums/EventStatus.java
@@ -0,0 +1,8 @@
+package com.weather_found.weather_app.modules.event.model.enums;
+
+public enum EventStatus {
+    SCHEDULED,
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELLED
+}
---
+++ src/main/java/com/weather_found/weather_app/modules/event/model/enums/EventType.java
@@ -0,0 +1,11 @@
+package com.weather_found.weather_app.modules.event.model.enums;
+
+public enum EventType {
+    CONCERT,
+    CONFERENCE,
+    MEETING,
+    WORKSHOP,
+    SPORTS,
+    FESTIVAL,
+    OTHER
+}
