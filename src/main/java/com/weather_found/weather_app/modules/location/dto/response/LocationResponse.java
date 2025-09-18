57987c7cb52e9aefa3fb350c019c95a533327be2
Omar Elrfaay <omar.tolis2004@gmail.com>
2025-09-18 21:42:35 +0300

    Add LocationResponse DTO for location API responses

---
+++ src/main/java/com/weather_found/weather_app/modules/location/dto/response/LocationResponse.java
@@ -0,0 +1,30 @@
+package com.weather_found.weather_app.modules.location.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * DTO for location response
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LocationResponse {
+
+    private Long id;
+    private String name;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+    private String country;
+    private String state;
+    private String city;
+    private String address;
+    private String timezone;
+    private Integer elevation;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
